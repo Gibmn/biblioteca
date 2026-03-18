2d12c9b1b96c28efeba1f05f50f1b3cfdccc6f54
naksdii <vini112341@gmail.com>
2026-03-18 13:54:36 -0300

    BAH

---
+++ src/main/java/Book.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class Book {
+
+    String name;
+    String author;
+    int ownerId;
+    int year;
+    boolean alugado = false;
+
+    Book(
+            String name,
+            String author,
+            int ownerId,
+            int year
+    ) {
+        this.name = name;
+        this.ownerId = ownerId;
+        this.author = author;
+        this.year = year;
+    }
+
+    public void alugar(ArrayList<User> users, int ownerId) {
+        this.ownerId = ownerId;
+        this.alugado = true;
+    }
+
+    public void devolução() {
+        this.alugado = false;
+    }
+}
